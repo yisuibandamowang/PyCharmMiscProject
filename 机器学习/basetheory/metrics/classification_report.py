a9d3b1922af5c2003508129361d63ecf21bfb029
我只有一岁半 <2307585872@qq.com>
2025-10-28 10:38:38 +0800

    feature: 分类评价指标综合案例

---
+++ 机器学习/basetheory/metrics/classification_report.py
@@ -0,0 +1,26 @@
+import numpy as np
+from sklearn.datasets import make_classification #自动生成分类数据集的函数
+from sklearn.model_selection import train_test_split #划分数据集
+from sklearn.metrics import classification_report #生成分类评估报告
+from sklearn.linear_model import LogisticRegression #逻辑回归模型
+
+# 生成数据集
+X,y = make_classification(n_samples=1000, n_features=20, n_classes=2,random_state=42)
+# print(X.shape)
+# print(y.shape)
+
+# 2. 划分数据集
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=42)
+
+# 3. 定义模型
+model = LogisticRegression()
+
+# 4. 训练模型
+model.fit(X_train,y_train)
+
+# 5. 预测结果
+y_pred = model.predict(X_test)
+
+# 6. 生成评估报告
+report = classification_report(y_test,y_pred)
+print(report)
