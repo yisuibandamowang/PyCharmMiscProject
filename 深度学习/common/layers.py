ae04346a5ab4baf9f1cd57db2f5772cbda7be2c4
我只有一岁半 <2307585872@qq.com>
2025-11-02 19:11:12 +0800

    feature: Affine 仿射层

--- 深度学习/common/layers.py
+++ 深度学习/common/layers.py
@@ -34,3 +34,30 @@ class sigmoid:
     def backward(self, dy):
         dx = dy * self.y * (1.0 - self.y)
         return dx
+
+
+# Affine 仿射层
+class Affine:
+    # 初始化
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        # 对输入数据 x 进行保存    方便方向传播计算梯度
+        self.x = None
+        self.orignal_x_shape = None
+        # 对权重 dw 和偏执参数 db 进行保存
+        self.dw = None
+        self.db = None
+    # 前向传播
+    def forward(self, X):
+        self.orignal_x_shape = X.shape
+        self.X = X.reshape(X.shape[0], -1)
+        y = np.dot(X, self.W) + self.b
+        return y
+    # 反向传播
+    def backward(self, dy):
+        dX = np.dot(dy, self.W.T)
+        dX = dX.reshape(*self.orignal_x_shape)
+        self.dW = np.dot(self.X.T, dy)
+        self.db = np.sum(dy, axis=0)
+        return dX
