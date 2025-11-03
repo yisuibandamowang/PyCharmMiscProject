b167edeef25ae3503b5200b13229363b83564692
我只有一岁半 <2307585872@qq.com>
2025-11-03 11:10:03 +0800

    feature: Adam 更新参数

--- 深度学习/common/optimizer.py
+++ 深度学习/common/optimizer.py
@@ -72,3 +72,34 @@ class RMSProp:
             self.h[key] *= self.decay
             self.h[key] += (1 - self.decay) * grads[key] * grads[key]
             params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-8)
+
+
+# Adam
+class Adam:
+    # 初始化
+    def __init__(self, lr=0.01, alpha1=0.9, alpha2=0.999):
+        self.lr = lr
+        self.alpha1 = alpha1
+        self.alpha2 = alpha2
+        self.v = None
+        self.h = None
+        self.t = 0  # 迭代次数
+    # 更新方法
+    def update(self, params, grads):
+        # 初始化v和h
+        if self.v is None:
+            self.v, self.h = {}, {}
+            for key, val in params.items():
+                self.v[key] = np.zeros_like(val)
+                self.h[key] = np.zeros_like(val)
+        self.t += 1 # 迭代次数+1
+        # 按照当前的迭代次数，改变学习率参数
+        lr_t = self.lr * np.sqrt(1 - self.alpha2**self.t) / (1 - self.alpha1**self.t)
+        # 遍历所有参数，按公式进行更新
+        for key in params.keys():
+            # self.v[key] = self.alpha1 * self.v[key] + (1 - self.alpha1) * grads[key]
+            # self.h[key] = self.alpha2 * self.h[key] + (1 - self.alpha2) * (grads[key] ** 2)
+            self.v[key] += (1 - self.alpha1) * (grads[key] - self.v[key])
+            self.h[key] += (1 - self.alpha2) * (grads[key] ** 2 - self.h[key])
+            params[key] -= lr_t * self.v[key] / (np.sqrt(self.h[key]) + 1e-8)
+
