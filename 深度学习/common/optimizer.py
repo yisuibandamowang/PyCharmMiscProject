991a5b0bd2d0543f0b57e9a7d3fd98a4bc6d7b66
我只有一岁半 <2307585872@qq.com>
2025-11-03 10:56:56 +0800

    feature: AdaGrad 更新参数

--- 深度学习/common/optimizer.py
+++ 深度学习/common/optimizer.py
@@ -1,30 +1,54 @@
 import numpy as np
+
+
 # 随机梯度下降 SGD
 class SGDR:
     # 初始化
     def __init__(self, lr=0.01):
         self.lr = lr
+
     # 参数更新 .  传入参数字典和梯度字典
     def update(self, params, grads):
         for key in params.keys():
             params[key] -= self.lr * grads[key]
 
 
-#动量法
+# 动量法
 class Momentum:
     # 初始化
     def __init__(self, lr=0.01, momentum=0.9):
         self.lr = lr
         self.momentum = momentum
-        self.v = None    #历史负梯度的加权和
+        self.v = None  # 历史负梯度的加权和
+
     # 参数更新
     def update(self, params, grads):
-        #初始化 v
+        # 初始化 v
         if self.v is None:
             self.v = {}
             for key, val in params.items():
                 self.v[key] = np.zeros_like(val)
-        #按照公式进行参数更新
+        # 按照公式进行参数更新
         for key in params.keys():
             self.v[key] = self.momentum * self.v[key] - self.lr * grads[key]
             params[key] += self.v[key]
+
+
+# AdaGrad
+class AdaGrad:
+    # 构造函数
+    def __init__(self, lr=0.01):
+        self.lr = lr
+        self.h = None
+
+    # 参数更新
+    def update(self, params, grads):
+        # 初始化 h
+        if self.h is None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+        # 按照公式进行参数更新
+        for key in params.keys():
+            self.h[key] += grads[key] * grads[key]
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-8)
