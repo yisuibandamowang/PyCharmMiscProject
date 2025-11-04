718942851541704f142c7e79ffef2d1d21c7b203
我只有一岁半 <2307585872@qq.com>
2025-11-04 09:56:50 +0800

    feature: pytorch 激活函数之 tanh(双曲正切函数) 函数

---
+++ 深度学习/07pytorch_deep_learning/activation_functions/2.tanh.py
@@ -0,0 +1,32 @@
+import torch
+import matplotlib.pyplot as plt
+
+x = torch.linspace(-5, 5, 1000, requires_grad=True)
+y = torch.tanh(x)
+
+# 画图
+fig, ax = plt.subplots(1, 2, figsize=(12, 4))
+# 1. 原函数图像
+ax[0].plot(x.data, y.data, "purple")
+ax[0].set_title("tanh(x)")
+ax[0].axhline(y=-1, color='gray', alpha=0.5, linewidth=1)
+ax[0].axhline(y=1, color='gray', alpha=0.5, linewidth=1)
+
+ax[0].spines['top'].set_visible(False)
+ax[0].spines['right'].set_visible(False)
+ax[0].spines['left'].set_position("zero")
+ax[0].spines['bottom'].set_position("zero")
+
+# 反向传播，计算x的梯度
+y.sum().backward()
+
+# 2. 导函数图像
+ax[1].plot(x.data, x.grad, "purple")
+ax[1].set_title("tanh'(x)")
+
+ax[1].spines['top'].set_visible(False)
+ax[1].spines['right'].set_visible(False)
+ax[1].spines['left'].set_position("zero")
+ax[1].spines['bottom'].set_position("zero")
+
+plt.show()
